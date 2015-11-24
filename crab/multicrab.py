bbbf24344f1b6190cebbb11138e4f490981db43b
Heiner Tholen <heiner.tholen@cern.ch>
2015-11-24 13:02:58 +0100

    adding crab files

---
+++ crab/multicrab.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+"""
+This is a small script that does the equivalent of multicrab.
+"""
+
+import os
+from optparse import OptionParser
+
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+
+def getOptions():
+    """
+    Parse and return the arguments provided by the user.
+    """
+    usage = ("Usage: %prog --crabCmd CMD [--workArea WAD --crabCmdOpts OPTS]"
+             "\nThe multicrab command executes 'crab CMD OPTS' for each project directory contained in WAD"
+             "\nUse multicrab -h for help")
+
+    parser = OptionParser(usage=usage)
+
+    parser.add_option('-c', '--crabCmd',
+                      dest = 'crabCmd',
+                      default = '',
+                      help = "crab command",
+                      metavar = 'CMD')
+
+    parser.add_option('-w', '--workArea',
+                      dest = 'workArea',
+                      default = '',
+                      help = "work area directory (only if CMD != 'submit')",
+                      metavar = 'WAD')
+
+    parser.add_option('-o', '--crabCmdOpts',
+                      dest = 'crabCmdOpts',
+                      default = '',
+                      help = "options for crab command CMD",
+                      metavar = 'OPTS')
+
+    (options, arguments) = parser.parse_args()
+
+    if arguments:
+        parser.error("Found positional argument(s): %s." % (arguments))
+    if not options.crabCmd:
+        parser.error("(-c CMD, --crabCmd=CMD) option not provided.")
+    if options.crabCmd != 'submit':
+        if not options.workArea:
+            parser.error("(-w WAR, --workArea=WAR) option not provided.")
+        if not os.path.isdir(options.workArea):
+            parser.error("'%s' is not a valid directory." % (options.workArea))
+
+    return options
+
+
+def main():
+
+    options = getOptions()
+
+    # The submit command needs special treatment.
+    if options.crabCmd == 'submit':
+
+        #--------------------------------------------------------
+        # The base config and inputDatasets is loaded from crab.py:
+        #--------------------------------------------------------
+        from crab import config, username, inputDatasets
+        #--------------------------------------------------------
+
+        for inDS in inputDatasets:
+            # inDS is of the form /A/B/C.
+            config.General.requestName = inDS.split('/')[1]
+            config.Data.inputDataset = inDS
+            config.Data.outputDatasetTag = 'out_%s' % config.General.requestName
+            config.Data.outLFNDirBase = '/store/user/%s/%s/%s/' % (
+                username, config.General.workArea, config.General.requestName)
+            # Submit.
+            try:
+                print "Submitting for input dataset %s" % (inDS)
+                crabCommand(options.crabCmd, config = config, *options.crabCmdOpts.split())
+            except HTTPException as hte:
+                print "Submission for input dataset %s failed: %s" % (inDS, hte.headers)
+            except ClientException as cle:
+                print "Submission for input dataset %s failed: %s" % (inDS, cle)
+
+    # All other commands can be simply executed.
+    elif options.workArea:
+
+        for dir in os.listdir(options.workArea):
+            projDir = os.path.join(options.workArea, dir)
+            if not os.path.isdir(projDir):
+                continue
+            # Execute the crab command.
+            msg = "Executing (the equivalent of): crab %s --dir %s %s" % (options.crabCmd, projDir, options.crabCmdOpts)
+            print "-"*len(msg)
+            print msg
+            print "-"*len(msg)
+            try:
+                crabCommand(options.crabCmd, dir = projDir, *options.crabCmdOpts.split())
+            except HTTPException as hte:
+                print "Failed executing command %s for task %s: %s" % (options.crabCmd, projDir, hte.headers)
+            except ClientException as cle:
+                print "Failed executing command %s for task %s: %s" % (options.crabCmd, projDir, cle)
+
+
+if __name__ == '__main__':
+    main()
+
